1ccd16e1354f33a41f3b875ae55a0376cb9ad56f
Ollie Petch <ollie.petch@digital.hmrc.gov.uk>
2016-12-20 11:49:18 +0000

    hardcode password

--- .ci_scripts/integrationTests.sh
+++ .ci_scripts/integrationTests.sh
@@ -23,7 +23,7 @@ fi
 if [ "$MONGO_PROFILE" = "mutual-ssl" ]; then
     MONGOSHELL_OPTS="$MONGOSHELL_OPTS --ssl --sslCAFile $SCRIPT_DIR/server.pem"
     MONGOSHELL_OPTS="$MONGOSHELL_OPTS --sslPEMKeyFile $SCRIPT_DIR/client.pem"
-    MONGOSHELL_OPTS="$MONGOSHELL_OPTS --sslPEMKeyPassword $PASS"
+    MONGOSHELL_OPTS="$MONGOSHELL_OPTS --sslPEMKeyPassword secret" # TODO pass as param
 fi
 
 MONGOSHELL_OPTS="$MONGOSHELL_OPTS --eval"
